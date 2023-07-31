e19b524c0e8b68647a7139c8460e2973c3f52cc8
Harsh-rm <harshu98@gmail.com>
2023-07-30 21:34:03 -0700

    NASA_Project commit

--- Projects/NASA_Project/server/src/app.js
+++ Projects/NASA_Project/server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -10,9 +11,15 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+
 app.use(express.json());
 // this middleware will parse any json data incoming from the body of the request
+app.use(express.static(path.join(__dirname, '..', 'public', )));
+
 app.use(planetsRouter);
 // app.listen();
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, ''));
+});
 
 module.exports = app;

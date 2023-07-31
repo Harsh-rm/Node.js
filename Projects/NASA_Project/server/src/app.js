ed68a00577dcc6328bb2ecc4687dea1e2d5dfd4f
Harsh-rm <harshu98@gmail.com>
2023-07-30 22:11:55 -0700

    NASA_Project commit

--- Projects/NASA_Project/server/src/app.js
+++ Projects/NASA_Project/server/src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '..', 'public', )));
 app.use(planetsRouter);
 // app.listen();
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, ''));
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 module.exports = app;

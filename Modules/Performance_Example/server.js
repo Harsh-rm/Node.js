1c81f238054ae83aa91bac3e3727cffee04f26ee
Harsh-rm <harshu98@gmail.com>
2023-08-02 22:37:52 -0700

    Performance Enhancement

--- Modules/Performance_Example/server.js
+++ Modules/Performance_Example/server.js
@@ -1,3 +1,4 @@
+//Performance Enhancement script
 const express = require('express');
 
 const app = express();
@@ -11,7 +12,21 @@ function delay(duration) {
 }
 
 // Real-life blocking functions
-// 
+/*
+    JSON.stringify({}) => returns a string representation of the object
+    JSON.parse("{}") => returns an object representation of the stringified object
+    calling these functions blocks the event loop of the server for a short period 
+    in large and complex programs this time can sprial out control and cause large delays
+
+    [5,1,3,2,4].sort() => Arrays.sort() function blocks the event loop, which would cause delays for large arrays
+
+    There are certain functions in node's built-in crypto module which are designed to execute slowly
+    so that it takes longer for hackers to guess a password or steal data ex: .pbkdf2() .scrypt() 
+    are password based key derivation functions that store user passwords in a hash format - https://nodejs.org/api/crypto.html
+
+    Research based on Response times in the Web Applications - https://www.nngroup.com/articles/response-times-3-important-limits/
+    Engagement based on response times - https://www.speedcurve.com/blog/web-performance-monitoring-user-engagement/
+*/
 
 app.get('/', (req, res) => {
     res.send('Performance example');
